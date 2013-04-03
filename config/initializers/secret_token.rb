5ee40907a36437f158645d9200ebd4d778d43704
John Shutt <john.d.shutt@gmail.com>
2013-04-03 13:23:24 -0700

    create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubHighlighter::Application.config.secret_token = 'bb9e6a66ed94605c4b65726918feb59dbb8ffea9baf083cfac85725bfaf4867350cfcb0ab4f1c5e58fa2819e9fb86fe6c55422d22346700f172959f06419de23'
