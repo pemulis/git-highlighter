148c65d8c3b9fa58455c2563d4b68b45c27ac17f
John Shutt <john.d.shutt@gmail.com>
2013-04-19 13:18:26 -0700

    add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 GithubHighlighter::Application.routes.draw do
+  get "users/new"
+
+  get "users/create"
+
+  get "users/update"
+
+  get "users/show"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
