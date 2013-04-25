af6b8a4fb40fdc14cc0c99648d73b4c58fa298bd
John Shutt <john.d.shutt@gmail.com>
2013-04-25 16:19:34 -0700

    okay, now let's try to grab some user information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,20 @@ class UsersController < ApplicationController
     @code = @params["code"]
 
     unless @code.nil?
-      @token = RestClient.post "https://github.com/login/oauth/access_token", client_id: ENV["CLIENT_ID"], client_secret: ENV["CLIENT_SECRET"], code: "#{@code}"
-      render
+      
+      resp = RestClient.post "https://github.com/login/oauth/access_token", client_id: ENV["CLIENT_ID"], client_secret: ENV["CLIENT_SECRET"], code: "#{@code}", accept: :json
+      
+      @token = resp["access_token"]
+      @client = Octokit::Client.new(oauth_token: @token)
+      @user = @client.user()
+      @following = @client.following(@user)
+      @user2 = Octokit.user("pemulis")
+      @following2 = Octokit.following("pemulis")
 
       # TODO:
       # X Grab access token from response
       # * Use access token to show correct user
+    
     end
   end
 
