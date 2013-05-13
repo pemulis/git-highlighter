73721f2080c6f21efd8b8155dbef7219d95fab62
John Shutt <john.d.shutt@gmail.com>
2013-05-12 19:52:37 -0700

    add get_starred_repos to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
 
     current_user.get_user_data(client)
     current_user.get_followed_users(client)
+    current_user.get_starred_repos(client)
     current_user.save
     redirect_to root_url 
   end
