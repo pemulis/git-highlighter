b0ba11143345896a126ecbd66e2a665aafa9bda1
John Shutt <john.d.shutt@gmail.com>
2013-06-05 12:26:40 -0700

    remove after_fork hook from resque initializer

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,4 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password, thread_safe: true)
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
-Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
+# Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
