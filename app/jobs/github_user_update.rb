6bd0e123e96af238db0aada6a53f906cc34e69bd
John Shutt <john.d.shutt@gmail.com>
2013-06-08 00:22:46 -0700

    add more output for tests

--- app/jobs/github_user_update.rb
+++ app/jobs/github_user_update.rb
@@ -6,19 +6,12 @@ class GithubUserUpdate < ActiveRecord::Base
   end
 
   def self.perform(login, oauth_token)
-    puts 'foo'
     client = Octokit::Client.new(login: login, oauth_token: oauth_token)
-    puts 'bar'
     current_user = User.find_by_login(login)
-    puts 'baz'
     current_user.get_user_data(client)
-    puts 'quz?'
     current_user.get_followed_users(client)
-    puts 'uhh'
     current_user.get_starred_repos(client)
-    puts 'some other thing'
     current_user.get_recommendations(client)
-    puts 'success???'
     
     # include something that tells users#updating that the job is done
 
