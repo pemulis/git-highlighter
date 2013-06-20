eaee907e1f19c8b7e5f52b0c500a46321a2c8447
John Shutt <john.d.shutt@gmail.com>
2013-06-20 16:47:15 -0700

    add js.erb file for ajax response handling

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -5,8 +5,17 @@ class RecommendationsController < ApplicationController
   def destroy
     if @rec = Recommendation.find(params[:id])
       @rec.hidden = true
-      @rec.save
-      redirect_to root_url
+      
+      respond_to do |format|
+        if @rec.save
+          format.html { redirect_to root_url }
+          format.js {}
+          format.json { render json: @rec, status: :hidden, location: @rec }
+        else
+          format.html { redirect_to root_url }
+          format.json { render json: @rec.errors, status: :not_hidden }
+        end
+      end
     end
   end
 end
