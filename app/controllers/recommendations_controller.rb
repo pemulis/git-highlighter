2c3abfb547b502a203187bc4b541ad85c1debfe0
John Shutt <john.d.shutt@gmail.com>
2013-06-20 14:21:19 -0700

    save hidden recommendations

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -5,6 +5,7 @@ class RecommendationsController < ApplicationController
   def destroy
     if @rec = Recommendation.find(params[:id])
       @rec.hidden = true
+      @rec.save
       redirect_to root_url
     end
   end
