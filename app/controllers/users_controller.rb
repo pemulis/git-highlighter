b5a8ba0263333c358a6d88d1822673039cc7bf82
John Shutt <john.d.shutt@gmail.com>
2013-06-24 14:45:46 -0700

    add update redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,6 @@ class UsersController < ApplicationController
     oauth_token = session[:oauth_token]
     update_job_id = GithubUserUpdate.create(login: login, oauth_token: oauth_token)
     session[:update_job_id] = update_job_id
+    redirect_to root_url
   end
 end
