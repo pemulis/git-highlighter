5fd5e016321e41dc640e56824fb13b576621da83
John Shutt <john.d.shutt@gmail.com>
2013-07-08 09:22:21 -0400

    rename status_hash helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def update_status
+  def status_hash 
     @status_hash = Resque::Plugins::Status::Hash.get(session[:update_job_id]) if session[:update_job_id]
   end
 end
