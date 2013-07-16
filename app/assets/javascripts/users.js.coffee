9d09f92f78f5a90be5e54dc569f8b35cc8a03c0d
John Shutt <john.d.shutt@gmail.com>
2013-07-16 16:00:39 -0400

    add some more ajax stuff

--- app/assets/javascripts/users.js.coffee
+++ app/assets/javascripts/users.js.coffee
@@ -2,9 +2,20 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
+# User sees the status of their recommendations updates dynamically
+#
+@UpdatePoller =
+  poll: ->
+    setTimeout @request, 1000
+
+  request: ->
+    $.post($('#update-status').data('url'))
+
 # User can mark that they're not interested in a recommendation, and 
 # it will never appear again.
 #
 $ ->
   $(".not-interested").on 'click', (event) =>
     $(event.currentTarget).closest(".recommendation").hide()
+  if $('#update-status').length > 0
+    UpdatePoller.poll()
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,16 @@ class UsersController < ApplicationController
   end
 
   def update
-    login = current_user.login
-    oauth_token = session[:oauth_token]
-    update_job_id = GithubUserUpdate.create(login: login, oauth_token: oauth_token)
-    session[:update_job_id] = update_job_id
+    if session[:update_job_id] == 100
+      session[:update_job_id] = nil
+
+    unless session[:update_job_id] do
+      login = current_user.login
+      oauth_token = session[:oauth_token]
+      update_job_id = GithubUserUpdate.create(login: login, oauth_token: oauth_token)
+      session[:update_job_id] = update_job_id
+    end
+
     respond_to do |format|
       format.html { redirect_to root_url }
       format.js
