72adbe36494bdb68020f57fb5725a0861a251c40
John Shutt <john.d.shutt@gmail.com>
2013-07-16 18:22:32 -0400

    change resque status hash expiration

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,3 @@
 Resque.redis = REDIS_WORKER
-Resque::Plugins::Status::Hash.expire_in = (24 * 60 * 60)
+Resque::Plugins::Status::Hash.expire_in = (15)
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
